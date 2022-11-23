7ff16a34a02939671c4ee74196e3f03aaa2190e8
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2022-11-23 17:48:29 -0300

    Criando os components extrato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ExtratoComponent } from './extrato/extrato.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, NovaTransferenciaComponent],
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
   imports: [BrowserModule, CommonModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/extrato/extrato.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extrato',
+  templateUrl: './extrato.component.html',
+})
+export class ExtratoComponent {}
