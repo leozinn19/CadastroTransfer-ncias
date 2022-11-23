ac421e0a6f3b82a67938566efa1cdd702fc5dbe5
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2022-11-23 18:30:15 -0300

    Colocando a data como: dia/mes/ano, ou seja mudando pra br

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import localePt from '@angular/common/locales/pt';
 
 import { ExtratoComponent } from './extrato/extrato.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { AppComponent } from './app.component';
 
+registerLocaleData(localePt, 'pt');
+
 @NgModule({
   declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
   imports: [BrowserModule, CommonModule, FormsModule],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
