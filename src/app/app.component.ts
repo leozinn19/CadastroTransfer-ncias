a0be2c0c1ddac0cf21a3920715aff7db16e9fd85
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2022-11-23 18:04:53 -0300

    Mudando transferencia para transferencias(lista) e implementando o *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CadastroTransferencia';
-  transferencia: any;
+  transferencias: any[] = [];
 
   cadastrar($event) {
     console.log($event);
-    this.transferencia = $event;
+    this.transferencias.push($event);
   }
 }
