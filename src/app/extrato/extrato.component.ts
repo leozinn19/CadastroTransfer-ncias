95a321e889df6a5eae57d84e8fb20e5bf4510bf8
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2022-11-23 22:43:53 -0300

    implementando o css no html

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
+  styleUrls: ['./extrato.component.css'],
 })
 export class ExtratoComponent {
   @Input() transferencias: any[];
